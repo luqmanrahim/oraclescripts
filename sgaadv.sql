3d659a4c3ac58d6c295e41db3e7f71f0798ed6e2
luqmanrahim <37920594+luqmanrahim@users.noreply.github.com>
2018-04-06 11:28:36 +0800

    Create sgaadv.sql

---
+++ sgaadv.sql
@@ -0,0 +1,4 @@
+col ESTD_PHYSICAL_READS for 999,999,999,999,999
+col ESTD_DB_TIME_FACTOR for 9.9999 heading "DB Time fctr"
+col SGA_SIZE_FACTOR for 9.9999 heading "SGA fctr"
+select * from gv$sga_target_advice order by inst_id, SGA_SIZE asc;
