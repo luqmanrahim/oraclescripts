f7cb29a23460982a14feeef050629822d38a1933
luqmanrahim <37920594+luqmanrahim@users.noreply.github.com>
2018-04-06 10:05:21 +0800

    Create size.sql

---
+++ size.sql
@@ -0,0 +1,7 @@
+select a.data_size+b.temp_size+c.redo_size "total_size (GB)"
+from ( select sum(bytes/1024/1024/1024) data_size
+         from dba_data_files ) a,
+     ( select nvl(sum(bytes/1024/1024/1024),0) temp_size
+         from dba_temp_files ) b,
+     ( select sum(bytes/1024/1024/1024) redo_size
+         from sys.v_$log ) c
