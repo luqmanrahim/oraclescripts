99c7633fa2137c2b2d232067ba3a20abe254cac5
luqmanrahim <37920594+luqmanrahim@users.noreply.github.com>
2018-04-06 11:29:31 +0800

    Update sgaadv.sql

--- sgaadv.sql
+++ sgaadv.sql
@@ -1,3 +1,4 @@
+set lines 160 pages 1000
 col ESTD_PHYSICAL_READS for 999,999,999,999,999
 col ESTD_DB_TIME_FACTOR for 9.9999 heading "DB Time fctr"
 col SGA_SIZE_FACTOR for 9.9999 heading "SGA fctr"
