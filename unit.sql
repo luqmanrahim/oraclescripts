011f153e4b708b66d4c7fafdf674b2e269925a49
luqmanrahim <37920594+luqmanrahim@users.noreply.github.com>
2018-04-19 16:01:09 +0800

    Update unit.sql

--- unit.sql
+++ unit.sql
@@ -5,3 +5,20 @@ create sequence fooseq start with 1 increment by 1;
 insert into foo values (fooseq.nextval, '10', 'X');
 
 insert into foo (select * from foo);
+
+
+
+---------------------------
+
+
+SQL> create table ziggy (id number, code number, date_created date, name varchar2(42));
+ 
+Table created.
+ 
+SQL> insert into ziggy select rownum, mod(rownum,100), sysdate – mod(rownum,1000), DAVID BOWIE’ from dual connect by level <= 2000000; 2000000 rows created. SQL> commit;
+ 
+Commit complete.
+ 
+SQL> exec dbms_stats.gather_table_stats(ownname=>user, tabname=>'ZIGGY', estimate_percent=>null, method_opt=>'FOR ALL COLUMNS SIZE 1');
+ 
+PL/SQL procedure successfully completed.
